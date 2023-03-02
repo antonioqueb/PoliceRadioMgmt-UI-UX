ef3d9f70f9c351e78550309c5d951f6b0d2f7d6a
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2023-03-02 14:28:57 -0600

    Create UI

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1 className='text-center mt-5'>Aqui va a estar la interface de control de servicios a radios de spsc</h1>
+    </div>
+  )
+}
+
+export default App
